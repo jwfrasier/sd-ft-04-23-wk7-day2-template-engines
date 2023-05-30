f3fa86524d485765fa1889c48e52f362dc582cf6
Joseph <frasier.joseph@gmail.com>
2023-05-30 14:20:45 -0500

    using css template from online to make ejs design

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,14 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const ejs = require("ejs");
 
-router.get("/user_data", (req, res) => {
-  let student = { name: "Jaye" };
-  //   const header = ;
-  let html = ejs.render(`<h1> Howdy <%= student.name; %> </h1>`, {
-    student: student,
-  });
-  res.send(html);
+router.get("/", (req, res) => {
+  res.render("./user/user.ejs", { user: { name: "Dustin" } });
+});
+
+router.get("/user_info", (req, res) => {
+  res.render("./user/info.ejs");
+});
+
+router.get("/user_about", (req, res) => {
+  res.render("./user/about.ejs");
+});
+
+router.get("/display_all_users", (req, res) => {
+  const userData = [
+    {
+      name: "Joe",
+      address: "123 main st",
+      city: "Tomball",
+      zipcode: 77733,
+    },
+    {
+      name: "Cameron",
+      address: "12312 main st",
+      city: "Atlanta",
+      zipcode: 23232,
+    },
+    {
+      name: "Matt",
+      address: "123 willow st",
+      city: "Houston",
+      zipcode: 72733,
+    },
+  ];
+
+  res.render("./user/displayAll.ejs", { userData: userData });
 });
 
 module.exports = router;
